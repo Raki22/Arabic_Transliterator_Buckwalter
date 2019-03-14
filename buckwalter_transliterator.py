f0541b5b8a59b92c803188a8e1d1f6ab9d636030
Raki LACHRAF <lachraf.raki@gmail.com>
2019-03-14 17:58:11 +0100

    Update buckwalter_transliterator.py

--- buckwalter_transliterator.py
+++ buckwalter_transliterator.py
@@ -5,10 +5,11 @@
 def BuckWalter_Transliterator(sentence, reverse=0):
     buckwalter_dictionary = {' ': ' ', '\u0621': "'", '\u0622': '|', '\u0623': '>', '\u0624': '&', '\u0625': '<', '\u0626': '}', '\u0627': 'A', '\u0628': 'b', '\u0629': 'p', '\u062A': 't', '\u062B': 'v', '\u062C': 'j', '\u062D': 'H', '\u062E': 'x', '\u062F': 'd', '\u0630': '*', '\u0631': 'r', '\u0632': 'z', '\u0633': 's', '\u0634': '$', '\u0635': 'S', '\u0636': 'D',
                              '\u0637': 'T', '\u0638': 'Z', '\u0639': 'E', '\u063A': 'g', '\u0640': '_', '\u0641': 'f', '\u0642': 'q', '\u0643': 'k', '\u0644': 'l', '\u0645': 'm', '\u0646': 'n', '\u0647': 'h', '\u0648': 'w', '\u0649': 'Y', '\u064A': 'y', '\u064B': 'F', '\u064C': 'N', '\u064D': 'K', '\u064E': 'a', '\u064F': 'u', '\u0650': 'i', '\u0651': '~', '\u0652': 'o'}
-
+    
     if reverse == 0:
         dict = buckwalter_dictionary
     elif reverse == 1:
+        # this code between brackets below inverts the whole dictionary in order to be able to achieve reverse transliteration 
         dict = {v: k for k, v in buckwalter_dictionary.items()}
     n_s = ""
     for char in sentence:
